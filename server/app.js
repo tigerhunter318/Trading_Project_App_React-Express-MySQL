f9ad67e485e44efc17e22bb86acb89c69733732d
Soufiane Amama <soufianeamama70@gmail.com>
2023-10-25 19:06:30 +0100

    first commit

--- server/app.js
+++ server/app.js
@@ -20,9 +20,6 @@ const database = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
   });
   
   database.connect((err, resul)=>{
